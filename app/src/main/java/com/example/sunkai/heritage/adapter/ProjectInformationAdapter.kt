f4b84558b9858da7065cdcbd369a12ec948531da
孙楷 <704725181@qq.com>
2020-03-03 14:20:27 +1100

    update：非遗列表内容补全
    1.非遗列表展示内容

--- app/src/main/java/com/example/sunkai/heritage/adapter/ProjectInformationAdapter.kt
+++ app/src/main/java/com/example/sunkai/heritage/adapter/ProjectInformationAdapter.kt
@@ -13,7 +13,12 @@ import com.example.sunkai.heritage.entity.response.ProjectListInformation
 
 class ProjectInformationAdapter(context: Context, data: List<ProjectListInformation>, glide: RequestManager) : BaseLoadMoreRecyclerAdapter<ProjectInformationAdapter.Holder, ProjectListInformation>(context, data, glide) {
     class Holder(view: View) : RecyclerView.ViewHolder(view) {
+        val number:TextView=view.findViewById(R.id.project_number)
         val title: TextView = view.findViewById(R.id.project_title)
+        val type:TextView=view.findViewById(R.id.project_type)
+        val category:TextView=view.findViewById(R.id.project_category)
+        val location:TextView=view.findViewById(R.id.project_location)
+        val time:TextView=view.findViewById(R.id.project_time)
     }
 
     override fun addNewData(datas: List<ProjectListInformation>) {
@@ -26,7 +31,12 @@ class ProjectInformationAdapter(context: Context, data: List<ProjectListInformat
     override fun onBindViewHolder(holder: Holder, position: Int) {
         super.onBindViewHolder(holder, position)
         val data=getItem(position)
+        holder.number.text=data.project_num
         holder.title.text=data.title
+        holder.category.text=data.cate
+        holder.type.text=data.type
+        holder.time.text=data.rx_time
+        holder.location.text=data.province
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
