f57cc77519af7e8c8569b0245a7e3fad60ab7aad
孙楷 <704725181@qq.com>
2020-03-03 17:05:56 +1100

    update：非遗项目详情页基础UI
    1.详情页基础数据填充

--- app/src/main/java/com/example/sunkai/heritage/activity/ProjectDetailActivity.kt
+++ app/src/main/java/com/example/sunkai/heritage/activity/ProjectDetailActivity.kt
@@ -3,11 +3,30 @@ package com.example.sunkai.heritage.activity
 import android.os.Bundle
 import com.example.sunkai.heritage.R
 import com.example.sunkai.heritage.activity.base.BaseGlideActivity
+import com.example.sunkai.heritage.entity.response.ProjectListInformation
+import com.example.sunkai.heritage.value.DATA
+import kotlinx.android.synthetic.main.activity_project_detail.*
 
 class ProjectDetailActivity : BaseGlideActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_detail)
+        initWidget()
     }
+
+    private fun initWidget() {
+        val data = intent.getSerializableExtra(DATA) as ProjectListInformation
+        projectDetailTitle.text=data.title
+        projectId.text=data.auto_id
+        projectNumber.text=data.num
+        projectTime.text=data.rx_time
+        projectType.text=data.type
+        projectLocation.text=data.province
+        projectCategory.text=data.cate
+        projectDeclaration.text=data.unit
+        projectDesc.text=data.content
+    }
+
+
 }
