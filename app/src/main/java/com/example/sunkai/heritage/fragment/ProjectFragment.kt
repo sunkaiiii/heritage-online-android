23d7f0970cab35139dde365de52c943d4acbb483
孙楷 <704725181@qq.com>
2020-03-06 03:15:28 +1100

    Update：添加搜索浮窗
    1.非遗项目页添加搜索浮窗

--- app/src/main/java/com/example/sunkai/heritage/fragment/ProjectFragment.kt
+++ app/src/main/java/com/example/sunkai/heritage/fragment/ProjectFragment.kt
@@ -11,7 +11,6 @@ import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
 import com.example.sunkai.heritage.R
 import com.example.sunkai.heritage.adapter.ProjectInformationAdapter
-import com.example.sunkai.heritage.adapter.baseAdapter.BaseLoadMoreRecyclerAdapter
 import com.example.sunkai.heritage.connectWebService.EHeritageApi
 import com.example.sunkai.heritage.connectWebService.RequestHelper
 import com.example.sunkai.heritage.entity.request.BasePathRequest
@@ -20,40 +19,41 @@ import com.example.sunkai.heritage.entity.response.ProjectListInformation
 import com.example.sunkai.heritage.fragment.baseFragment.BaseLazyLoadFragment
 import com.example.sunkai.heritage.interfaces.RequestAction
 import com.example.sunkai.heritage.tools.OnSrollHelper
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.fragment_project.*
 
 class ProjectFragment : BaseLazyLoadFragment() {
-    private var pageNumber=1
+    private var pageNumber = 1
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_project, container, false)
     }
 
     override fun startLoadInformation() {
-        requestHttp(EHeritageApi.GetProjectBasicInformation )
+        requestHttp(EHeritageApi.GetProjectBasicInformation)
     }
 
     override fun onRestoreFragmentLoadInformation() {}
 
     override fun onTaskReturned(api: RequestHelper, action: RequestAction, response: String) {
-        when(api.getRequestApi())
-        {
-            EHeritageApi.GetProjectBasicInformation->
-            {
-                val projectInformation=fromJsonToObject(response,ProjectBasicInformation::class.java)
+        when (api.getRequestApi()) {
+            EHeritageApi.GetProjectBasicInformation -> {
+                val projectInformation = fromJsonToObject(response, ProjectBasicInformation::class.java)
                 FillProjectBasicDataIntoView(projectInformation)
                 loadProjectList()
             }
-            EHeritageApi.GetHeritageProjectList->{
-                val projectListData=fromJsonToList(response,ProjectListInformation::class.java)
-                var adapter=ProjectInformationList.adapter
-                if(adapter==null)
-                {
-                    adapter=ProjectInformationAdapter(context!!,projectListData,glide)
-                    ProjectInformationList.adapter=adapter
+            EHeritageApi.GetHeritageProjectList -> {
+                val projectListData = fromJsonToList(response, ProjectListInformation::class.java)
+                var adapter = ProjectInformationList.adapter
+                if (adapter == null) {
+                    adapter = ProjectInformationAdapter(context!!, projectListData, glide)
+                    ProjectInformationList.adapter = adapter
+                    fragmentProjectAppBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, _ ->
+                        fragmentProjectSearchCard.visibility = if (ProjectInformationList.y == 0f) View.VISIBLE else View.GONE
+                    })
+                    ProjectInformationList.addOnScrollListener(onScrollDirectionHelper)
                     ProjectInformationList.addOnScrollListener(onScrollHelper)
-                }else{
-                    if(adapter is ProjectInformationAdapter)
-                    {
+                } else {
+                    if (adapter is ProjectInformationAdapter) {
                         adapter.addNewData(projectListData)
                         onScrollHelper.setPageLoaded()
                     }
@@ -63,16 +63,25 @@ class ProjectFragment : BaseLazyLoadFragment() {
         }
     }
 
-    private val onScrollHelper= object: OnSrollHelper()
-    {
+    private val onScrollHelper = object : OnSrollHelper() {
         override fun loadMoreData(recyclerView: RecyclerView) {
             loadProjectList()
         }
     }
 
-    private fun loadProjectList()
-    {
-        requestHttp(EHeritageApi.GetHeritageProjectList,object :BasePathRequest(){
+    private val onScrollDirectionHelper = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            fragmentProjectSearchCard.visibility = if (recyclerView.y == 0f) View.VISIBLE else View.GONE
+//            if (dy < 0 && fragmentProjectSearchCard.y <= Utils.dip2px(32)) {
+//                fragmentProjectSearchCard.y = fragmentProjectSearchCard.y - dy
+//            } else if (dy > 0 && fragmentProjectSearchCard.y + fragmentProjectSearchCard.height >= 0) {
+//                fragmentProjectSearchCard.y = fragmentProjectSearchCard.y - dy
+//            }
+        }
+    }
+
+    private fun loadProjectList() {
+        requestHttp(EHeritageApi.GetHeritageProjectList, object : BasePathRequest() {
             override fun getPathParamerater(): List<String> {
                 return listOf(pageNumber++.toString())
             }
@@ -80,25 +89,24 @@ class ProjectFragment : BaseLazyLoadFragment() {
     }
 
     private fun FillProjectBasicDataIntoView(projectInformation: ProjectBasicInformation) {
-        ProjectDescLoading.visibility=View.GONE
-        ProjectPageLayout.visibility=View.VISIBLE
-        ProjectPageTitle.text=projectInformation.title
-        ProjectPageContent.text=projectInformation.content
+        ProjectDescLoading.visibility = View.GONE
+        ProjectPageLayout.visibility = View.VISIBLE
+        ProjectPageTitle.text = projectInformation.title
+        ProjectPageContent.text = projectInformation.content
         ProjectShowContent.setOnClickListener {
-            val animation=ChangeBounds()
-            animation.duration=300
-            TransitionManager.beginDelayedTransition(ProjectPageLayout,animation)
-            ProjectPageContent.maxLines=if(ProjectPageContent.maxLines==999)3 else 999
+            val animation = ChangeBounds()
+            animation.duration = 300
+            TransitionManager.beginDelayedTransition(ProjectPageLayout, animation)
+            ProjectPageContent.maxLines = if (ProjectPageContent.maxLines == 999) 3 else 999
         }
         projectInformation.numItem.forEach {
-            val itemLayout=LayoutInflater.from(context).inflate(R.layout.fragment_project_item_layout,ProjectDescLayout,false)
-            itemLayout.findViewById<TextView>(R.id.FragmentProjectItemTitle).text=it.desc
-            itemLayout.findViewById<TextView>(R.id.FragmentProjectItemContent).text=it.num
-            val layoutParams=itemLayout.layoutParams
-            if(layoutParams is LinearLayout.LayoutParams)
-            {
-                layoutParams.weight=1.0f
-                itemLayout.layoutParams=layoutParams
+            val itemLayout = LayoutInflater.from(context).inflate(R.layout.fragment_project_item_layout, ProjectDescLayout, false)
+            itemLayout.findViewById<TextView>(R.id.FragmentProjectItemTitle).text = it.desc
+            itemLayout.findViewById<TextView>(R.id.FragmentProjectItemContent).text = it.num
+            val layoutParams = itemLayout.layoutParams
+            if (layoutParams is LinearLayout.LayoutParams) {
+                layoutParams.weight = 1.0f
+                itemLayout.layoutParams = layoutParams
             }
             ProjectDescLayout.addView(itemLayout)
         }
