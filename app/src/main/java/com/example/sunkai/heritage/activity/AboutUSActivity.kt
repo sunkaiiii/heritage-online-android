de3041e499449c526e66ac3bf12efcd9f841c661
Kai Sun <ksun0017@qq.com>
2021-08-11 01:20:55 +1000

    关于我们，Compose
    1.引入jetpack compose
    2.关于我们页面元素替换为compose实现

--- app/src/main/java/com/example/sunkai/heritage/activity/AboutUSActivity.kt
+++ app/src/main/java/com/example/sunkai/heritage/activity/AboutUSActivity.kt
@@ -4,7 +4,24 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.ActionBar
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.sunkai.heritage.R
 import com.example.sunkai.heritage.activity.base.BaseGlideActivity
 import com.example.sunkai.heritage.databinding.ActivityAboutUsBinding
 import com.example.sunkai.heritage.dialog.LicenceDialog
@@ -21,16 +38,35 @@ class AboutUSActivity : BaseGlideActivity() {
         setContentView(binding.root)
         val actionBack: ActionBar? = supportActionBar
         actionBack?.setDisplayHomeAsUpEnabled(true)
-        binding.licenceTextView.setOnClickListener {
-            LicenceDialog().show(supportFragmentManager, "licence")
+        binding.baseVersion.setContent {
+            AboutUsCard()
         }
-        binding.baseGitUrl.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(binding.baseGitUrl.text.toString()))
-            startActivity(intent)
-        }
-
-        binding.baseVersion.text = VERSION_NAME()
     }
+
+    @Composable
+    @Preview
+    fun AboutUsCard() {
+        val gitUrl = getString(R.string.program_git_url)
+        val margin = Modifier.padding(Dp(8f))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher),
+                contentDescription = getString(R.string.logo),
+                Modifier.size(48.dp,48.dp).padding(0.dp, 0.dp, 0.dp, 12.dp)
+            )
+            Text(text = VERSION_NAME())
+            ClickableText(text = AnnotatedString(gitUrl), onClick = {
+                val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(gitUrl))
+                startActivity(intent)
+            }, modifier = margin)
+            ClickableText(text = AnnotatedString(getString(R.string.licence)), onClick = {
+                LicenceDialog().show(supportFragmentManager, "licence")
+            })
+        }
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
